f14369ef7714087f2700270d365d6195a75ba8a3
Alexander Barth <barth.alexander@gmail.com>
2021-05-12 22:03:34 +0200

    add tutorial.jl tutorial.ipynb

---
+++ tutorial.jl
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,jl:percent
+#     text_representation:
+#       extension: .jl
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.9.1
+#   kernelspec:
+#     display_name: Julia 1.6.0
+#     language: julia
+#     name: julia-1.6
+# ---
+
+# %% [markdown]
+# # Tutorial on model output analysis and visualization
+
+# %% [markdown]
+# ## Installation and setup
+
+
+# %% [markdown]
+# ## Download some example data
+
+# %% [markdown]
+# ## Vizualize surface data
+
+# %% [markdown]
+# ## Vizualize horizontal sections data
+
+# %% [markdown]
+# ## Vizualize vertical sections data
+
+# %% [markdown]
+# ## Compute integrals and averages
+
+# %% [markdown]
+# ## Validate the model with in situ observations and satellite observations
+
+# %% [markdown]
+# ## Compute Empirical Orthogonal Functions (EOFs)
+
+# %% [markdown]
+# ## Compute derived quantities (Brunt-Väisälä frequency, potential temperature, absolute salinity)
+
+# %% [markdown]
+# ## Drifter trajectories
